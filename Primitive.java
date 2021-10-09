d5745932b141c7cf2496c6d3a54af50b89096c28
Suhail-523 <suhail.m@gmail.com>
2021-10-09 17:41:24 +0530

    First Commit

---
+++ Primitive.java
@@ -0,0 +1,31 @@
+package coreJava;
+
+public class Primitive {
+
+	public static void main(String args [])
+	
+	{
+		char a = 'A';
+		
+		byte b = 5;
+		
+		short c = 10;
+		
+		int d = 45;
+		
+		float e = 4.55787f;
+		
+		double f = 5.34553344d;
+		
+		boolean g = true;
+		 
+		
+		System.out.println("char:" + a);
+		System.out.println("byte:" + b);
+		System.out.println("short:" + c);
+		System.out.println("int:" + d);
+		System.out.println("float:" + e);
+		System.out.println("double:" + f);
+		System.out.println("boolean:" + g);
+	}
+}
---
+++ WhileDemo.java
@@ -0,0 +1,15 @@
+package coreJava;
+
+public class WhileDemo {
+	
+	public static void main(String args[]) {
+		
+		int count = 1;
+		
+		while (count < 10) {
+			System.out.println("Count is: " + count);
+			count++;
+		}
+	}
+
+}
